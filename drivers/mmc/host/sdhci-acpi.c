b6b5eb962964040f09a6c6d5282140ea1ab931e0
Chew, Kean Ho <kean.ho.chew@intel.com>
2013-12-23 16:14:27 +0800

    mmc: sdhci: Force BYT SDCARD host to run with SDR25 mode
    The clock appears to be unstable when SDCARD host running with
    DDR50 mode, thus causing CRC issue. This is to introduce a new
    quirk to force host with broken DDR50 mode to run with SDR25
    mode.
    
    Signed-off-by: Chew, Kean Ho <kean.ho.chew@intel.com>
    Signed-off-by: Chew, Chiau Ee <chiau.ee.chew@intel.com>
    Signed-off-by: Maurice Petallo <mauricex.r.petallo@intel.com>

--- drivers/mmc/host/sdhci-acpi.c
+++ drivers/mmc/host/sdhci-acpi.c
@@ -134,7 +134,8 @@ static const struct sdhci_acpi_slot sdhci_acpi_slot_int_sdio = {
 static const struct sdhci_acpi_slot sdhci_acpi_slot_int_sd = {
 	.flags   = SDHCI_ACPI_SD_CD | SDHCI_ACPI_SD_CD_OVERRIDE_LEVEL |
 		   SDHCI_ACPI_RUNTIME_PM,
-	.quirks2 = SDHCI_QUIRK2_CARD_ON_NEEDS_BUS_ON,
+	.quirks2 = SDHCI_QUIRK2_CARD_ON_NEEDS_BUS_ON |
+		   SDHCI_QUIRK2_BROKEN_DDR50,
 };
 
 struct sdhci_acpi_uid_slot {
