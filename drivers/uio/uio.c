ac5ece1c0c741cf1c226e769c39027b554c7fcbe
Bin Jiang <bin.jiang@windriver.com>
2014-02-18 15:17:09 +0800

    uio: align uio memory region
    The uio memory region should be at page boundaries in order for
    user mmap() to work. The region size is rounded to a multiple of
    PAGE_SIZE.
    
    Signed-off-by: Bin Jiang <bin.jiang@windriver.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>
    Signed-off-by: Weiwei Wang <weiwei.wang@windriver.com>

--- drivers/uio/uio.c
+++ drivers/uio/uio.c
@@ -801,6 +801,22 @@ static void release_uio_class(void)
 	uio_major_cleanup();
 }
 
+static void uio_mem_region_align(struct uio_info *info)
+{
+	int mi;
+	struct uio_mem *mem;
+
+	for (mi = 0; mi < MAX_UIO_MAPS; mi++) {
+		mem = &info->mem[mi];
+		if (mem->size == 0)
+			break;
+		if (mem->memtype == UIO_MEM_PHYS) {
+			mem->addr = mem->addr & PAGE_MASK;
+			mem->size = PAGE_ALIGN(mem->size);
+		}
+	}
+}
+
 /**
  * uio_register_device - register a new userspace IO device
  * @owner:	module that creates the new device
@@ -818,6 +834,8 @@ int __uio_register_device(struct module *owner,
 
 	if (!parent || !info || !info->name || !info->version)
 		return -EINVAL;
+
+	uio_mem_region_align(info);
 
 	info->uio_dev = NULL;
 
