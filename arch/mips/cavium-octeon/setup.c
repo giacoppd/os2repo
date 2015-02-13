bc336512658205471b6bf352d53d631f27c9dc5e
Abhishek Paliwal <abhishek.paliwal@aricent.com>
2015-02-13 14:28:01 +0530

    MIPS: OCTEON: Print address of passed device tree.
    From: David Daney <david.daney@cavium.com>
    
    This allows us to verify the bootloader is doing what we ask.
    
    MIPS: OCTEON: Change load address to waste less memory.
    
    "Modern" octeon bootloaders allow us to load the image at 0x100000.
    Moving there from 0x1100000 makes 16MB more memory usable.
    
    MIPS: OCTEON Handle bootloader structures in little-endian mode.
    
    Compensate for the differences in the layout of in-memory bootloader
    information as seen from little-endian mode.
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Abhishek Paliwal <abhishek.paliwal@aricent.com>

--- arch/mips/cavium-octeon/setup.c
+++ arch/mips/cavium-octeon/setup.c
@@ -1394,6 +1394,7 @@ void __init device_tree_init(void)
 
 	if (octeon_bootinfo->minor_version >= 3 && octeon_bootinfo->fdt_addr) {
 		fdt = phys_to_virt(octeon_bootinfo->fdt_addr);
+		pr_info("Using passed Device Tree <%p>.\n", fdt);
 		if (fdt_check_header(fdt))
 			panic("Corrupt Device Tree passed to kernel.");
 		dt_size = be32_to_cpu(fdt->totalsize);
@@ -1417,8 +1418,6 @@ void __init device_tree_init(void)
 	if (do_prune) {
 		octeon_prune_device_tree();
 		pr_info("Using internal Device Tree.\n");
-	} else {
-		pr_info("Using passed Device Tree.\n");
 	}
 	unflatten_device_tree();
 }
