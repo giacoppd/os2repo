f5fc131b3e4413f197d3401acea29d6ca8f60ceb
Abhishek Paliwal <abhishek.paliwal@aricent.com>
2015-02-13 15:32:12 +0530

    MIPS: Only flush local ICache in get_new_asid().
    From: David Daney <david.daney@cavium.com>
    
    For virtually tagged ICache, on ASID rollover, only the local ICache
    needs to be invalidated.  Flushing across all CPUs is unnecessary.
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Abhishek Paliwal <abhishek.paliwal@aricent.com>

--- arch/mips/include/asm/mmu_context.h
+++ arch/mips/include/asm/mmu_context.h
@@ -102,7 +102,7 @@ get_new_asid(unsigned long cpu)
 
 	if (! ((asid += ASID_INC) & ASID_MASK) ) {
 		if (cpu_has_vtag_icache)
-			flush_icache_all();
+			local_flush_icache_all();
 #if IS_ENABLED(CONFIG_KVM_MIPS_TE)
 		kvm_local_flush_tlb_all();      /* start new asid cycle */
 #else
