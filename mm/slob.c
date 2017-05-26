bab23e74b2f6bee2c5a43c0b8bdcf9faba27e381
dominic <giacoppd@oregonstate.edu>
2017-05-26 15:49:51 -0700

    Changed page alloc, in theory it now does best fit for blocks on that page. I think.

--- mm/slob.c
+++ mm/slob.c
@@ -218,7 +218,9 @@ static void *slob_page_alloc(struct page *sp, size_t size, int align)
 {
 	slob_t *prev, *cur, *aligned = NULL;
 	int delta = 0, units = SLOB_UNITS(size);
-
+	int bdelta = 0;
+	slob_t *curbp, *curb, *curba , *curbn = NULL; //markers for best position
+	slobidx_t curbest = 9999999; //best fit, with a hueg number so it triggers on first pass
 	for (prev = NULL, cur = sp->freelist; ; prev = cur, cur = slob_next(cur)) {
 		slobidx_t avail = slob_units(cur);
 
@@ -226,40 +228,65 @@ static void *slob_page_alloc(struct page *sp, size_t size, int align)
 			aligned = (slob_t *)ALIGN((unsigned long)cur, align);
 			delta = aligned - cur;
 		}
-		if (avail >= units + delta) { /* room enough? */
-			slob_t *next;
-
-			if (delta) { /* need to fragment head to align? */
-				next = slob_next(cur);
+		if (avail == units + delta) //if exact fit, stop looking and plop here.
+		{
+			slob_t *next;		
+			next = slob_next(cur);
+			if (delta) { /* shouldn't happen but can't hurt */
 				set_slob(aligned, avail - delta, next);
 				set_slob(cur, delta, aligned);
 				prev = cur;
 				cur = aligned;
 				avail = slob_units(cur);
 			}
+			if (prev)
+				set_slob(prev, slob_units(prev), next);
+			else
+				sp->freelist = next;		
+			set_slob(cur + units, avail - units, next);
+			sp->units -= units;
+			if (!sp->units)
+				clear_slob_page_free(sp);
+			return cur;
 
-			next = slob_next(cur);
-			if (avail == units) { /* exact fit? unlink. */
-				if (prev)
-					set_slob(prev, slob_units(prev), next);
-				else
-					sp->freelist = next;
-			} else { /* fragment */
-				if (prev)
-					set_slob(prev, slob_units(prev), cur + units);
+		}
+		if (avail > units + delta && (avail - (units + delta) < curbest)) { 
+
+			curbp = prev;
+			curb = cur;
+			curba = aligned;
+			bdelta = delta;
+			curbest = avail - (units + delta);
+			}
+
+			if(slob_last(cur)) {
+				if(curb != NULL) {
+				slobidx_t curbav = slob_units(curb);
+				curbn = slob_next(curb);
+
+				if (bdelta) { /* need to fragment head to align? */
+					set_slob(curba, curbav - bdelta, curbn);
+					set_slob(curb, bdelta, curba);
+					curbp = curb;
+					curb = curba;
+					curbav = slob_units(curb);
+				}			
+				/* fragment */
+				if (curbp)
+					set_slob(curbp, slob_units(curbp), curb + units);
 				else
 					sp->freelist = cur + units;
-				set_slob(cur + units, avail - units, next);
+				set_slob(curb + units, curbava - units, curbn);
 			}
 
 			sp->units -= units;
 			if (!sp->units)
 				clear_slob_page_free(sp);
-			return cur;
+			return curb;
+			}
 		}
-		if (slob_last(cur))
-			return NULL;
 	}
+	return NULL;
 }
 
 /*
@@ -287,7 +314,9 @@ static void *slob_alloc(size_t size, gfp_t gfp, int align, int node)
 		/*
 		 * If there's a node specification, search for a partial
 		 * page with a matching node id in the freelist.
-		 */
+		 *
+		 * Dominic: I have no idea what this does
+		 * /
 		if (node != NUMA_NO_NODE && page_to_nid(sp) != node)
 			continue;
 #endif
@@ -312,6 +341,7 @@ static void *slob_alloc(size_t size, gfp_t gfp, int align, int node)
 	spin_unlock_irqrestore(&slob_lock, flags);
 
 	/* Not enough space: must allocate a new page */
+	//no touch methinks
 	if (!b) {
 		b = slob_new_pages(gfp & ~__GFP_ZERO, 0, node);
 		if (!b)
