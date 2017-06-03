7a52ccd1179b010c6027fa35feee28b006822c06
dominic <giacoppd@oregonstate.edu>
2017-06-03 15:52:04 -0700

    added syscall stuff

--- mm/slob.c
+++ mm/slob.c
@@ -70,6 +70,8 @@
 #include <linux/syscalls.h>
 #include <trace/events/kmem.h>
 
+#define get_free syscall(353)
+#define get_claimed syscall(354)
 #include <linux/linkage.h>
 //for asm stuff
 #include <linux/atomic.h>
@@ -329,7 +331,12 @@ static void *slob_alloc(size_t size, gfp_t gfp, int align, int node)
     list_for_each_entry(sp, temp, list) {
         free_units += sp->units;
     }
-
+        float fragmented;
+        printk("Our syscalls for project 4...\n");
+        fragmented = (float)get_free / (float)get_claimed;
+        printk("Free Memory: %i bytes\n", get_free);
+        printk("Claimed Memory: %i bytes\n", get_claimed);
+        printk("Fragmented: %f\n", fragmented);
 	spin_unlock_irqrestore(&slob_lock, flags);
 
 	/* Not enough space: must allocate a new page */
